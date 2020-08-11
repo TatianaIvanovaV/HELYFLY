dee4ef8794a5b8ee29b464825e2f2eec76100ba1
Tatiana Ivanova <komarivanova@gmail.com>
2020-08-11 13:37:21 +0300

    + jquery

---
+++ src/js/main.js
@@ -0,0 +1,32 @@
+/*var galleryItem = docyment.querySelector(".gallery-group__item");
+var openGallery = document.querySelector('.gallery-show__content');
+var galleryGroup = document.querySelector('.gallery-group');
+
+function ri() {
+  if (window.matchMedia("(max-width: 900px)").matches) {
+    
+    galleryGroup.classList.add("items4")
+  } else if (window.matchMedia("(max-width: 650px)").matches) {
+    
+    galleryGroup.classList.add("items3")
+  }
+}
+
+openGallery.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  if (window.matchMedia("(max-width: 900px)").matches) {
+    
+    galleryGroup.classList.remove("items4")
+  } else if (window.matchMedia("(max-width: 650px)").matches) {
+    
+    galleryGroup.classList.remove("items3")
+  }
+   else {}
+});*/
+
+/*$(function() {
+  $('.open-scr').click(function() {
+      $('.scr-box-hide').toggleClass('expand');
+      $(".open-scr").remove();
+  });
+});*/
