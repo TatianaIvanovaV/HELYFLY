ae8a48571e90a6bd3af4aeb6845c853de573e7b1
Tatiana Ivanova <komarivanova@gmail.com>
2020-08-15 23:11:55 +0300

    +gallery-group.styl

--- src/js/main.js
+++ src/js/main.js
@@ -1,68 +1,23 @@
 
 'use strict';
 
+/*---  video-player ---*/
   $('#play-video').on('click', function(e) {
     e.preventDefault();
     $("#video")[0].src += "&autoplay=0";
-    $(this).hide();
-    
+    $(this).hide();   
   });
 
-/*$('.offer-video__content').on('click', function(e) {
-	e.preventDefault();
-	
-	var self = $(this);
-	var videoSrc = self.attr('href');
-	var videoId = videoSrc.substr(videoSrc.length - 11) + '?rel=0&autoplay=1';
-	
-  self.find('img').css('z-index', '0');
-	self.find('iframe').attr('src', 'https://www.youtube.com/embed/' + videoId);
-
-});*/
-
-
-/*$('.video__a').on('click', function(e) {
-	e.preventDefault();
-	
-	var self = $(this);
-	var videoSrc = self.attr('href');
-	var videoId = videoSrc.substr(videoSrc.length - 11) + '?rel=0&autoplay=1';
-	
-	self.find('img').css('z-index', '0');
-	self.find('iframe').attr('src', 'https://www.youtube.com/embed/' + videoId);
-
-});*/
-
-/*var galleryItem = docyment.querySelector(".gallery-group__item");
-var openGallery = document.querySelector('.gallery-show__content');
-var galleryGroup = document.querySelector('.gallery-group');
-
-function ri() {
-  if (window.matchMedia("(max-width: 900px)").matches) {
-    
-    galleryGroup.classList.add("items4")
-  } else if (window.matchMedia("(max-width: 650px)").matches) {
-    
-    galleryGroup.classList.add("items3")
-  }
-}
-
-openGallery.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  if (window.matchMedia("(max-width: 900px)").matches) {
-    
-    galleryGroup.classList.remove("items4")
-  } else if (window.matchMedia("(max-width: 650px)").matches) {
-    
-    galleryGroup.classList.remove("items3")
-  }
-   else {}
-});*/
-
-/*$(function() {
-  $('.open-scr').click(function() {
-      $('.scr-box-hide').toggleClass('expand');
-      $(".open-scr").remove();
-  });
-});*/
-
+/*---  gallery-images --- */ 
+  $('.gallery__span').click(function(){
+    $('.arrow-img').toggleClass('up');
+    $('.gallery__item').toggleClass('expand');
+    if (!$(this).data('status')) {
+      $(this).html('Скрыть фото');
+      $(this).data('status', true);
+    }
+    else {
+      $(this).html('Показать все фото');
+      $(this).data('status', false);
+    }
+});
